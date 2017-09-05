cf84030149266058d32a2523d09a20c1b8ed5141
Hazelin Ngan <hazelin.ngan@gmail.com>
2017-09-05 13:06:58 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = newObject({artistName: "songTitle"})
+var playlist = new Object({artistName: "songTitle"})
 
 function updatePlaylist (playlist, artistName, songTitle){
   playlist[artistName]=songTitle;
